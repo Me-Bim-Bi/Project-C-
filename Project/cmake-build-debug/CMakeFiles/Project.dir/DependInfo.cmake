bf6df372fb92f20e635894902cdf25cf05fe9749
Thanh <thanhhachu87@gmail.com>
2025-01-08 09:47:16 +0100

    edit power point file

---
+++ Project/cmake-build-debug/CMakeFiles/Project.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/chuhathanh/Workspaces/Project-C-/Project/Clothing.cpp" "CMakeFiles/Project.dir/Clothing.cpp.o" "gcc" "CMakeFiles/Project.dir/Clothing.cpp.o.d"
+  "/Users/chuhathanh/Workspaces/Project-C-/Project/Cosmetic.cpp" "CMakeFiles/Project.dir/Cosmetic.cpp.o" "gcc" "CMakeFiles/Project.dir/Cosmetic.cpp.o.d"
+  "/Users/chuhathanh/Workspaces/Project-C-/Project/Employee.cpp" "CMakeFiles/Project.dir/Employee.cpp.o" "gcc" "CMakeFiles/Project.dir/Employee.cpp.o.d"
+  "/Users/chuhathanh/Workspaces/Project-C-/Project/EmployeeHandler.cpp" "CMakeFiles/Project.dir/EmployeeHandler.cpp.o" "gcc" "CMakeFiles/Project.dir/EmployeeHandler.cpp.o.d"
+  "/Users/chuhathanh/Workspaces/Project-C-/Project/Management.cpp" "CMakeFiles/Project.dir/Management.cpp.o" "gcc" "CMakeFiles/Project.dir/Management.cpp.o.d"
+  "/Users/chuhathanh/Workspaces/Project-C-/Project/Menu.cpp" "CMakeFiles/Project.dir/Menu.cpp.o" "gcc" "CMakeFiles/Project.dir/Menu.cpp.o.d"
+  "/Users/chuhathanh/Workspaces/Project-C-/Project/Product.cpp" "CMakeFiles/Project.dir/Product.cpp.o" "gcc" "CMakeFiles/Project.dir/Product.cpp.o.d"
+  "/Users/chuhathanh/Workspaces/Project-C-/Project/ProductHandler.cpp" "CMakeFiles/Project.dir/ProductHandler.cpp.o" "gcc" "CMakeFiles/Project.dir/ProductHandler.cpp.o.d"
+  "/Users/chuhathanh/Workspaces/Project-C-/Project/main.cpp" "CMakeFiles/Project.dir/main.cpp.o" "gcc" "CMakeFiles/Project.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Project/cmake-build-debug/CMakeFiles/Project.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project.dir/Clothing.cpp.o"
+  "CMakeFiles/Project.dir/Clothing.cpp.o.d"
+  "CMakeFiles/Project.dir/Cosmetic.cpp.o"
+  "CMakeFiles/Project.dir/Cosmetic.cpp.o.d"
+  "CMakeFiles/Project.dir/Employee.cpp.o"
+  "CMakeFiles/Project.dir/Employee.cpp.o.d"
+  "CMakeFiles/Project.dir/EmployeeHandler.cpp.o"
+  "CMakeFiles/Project.dir/EmployeeHandler.cpp.o.d"
+  "CMakeFiles/Project.dir/Management.cpp.o"
+  "CMakeFiles/Project.dir/Management.cpp.o.d"
+  "CMakeFiles/Project.dir/Menu.cpp.o"
+  "CMakeFiles/Project.dir/Menu.cpp.o.d"
+  "CMakeFiles/Project.dir/Product.cpp.o"
+  "CMakeFiles/Project.dir/Product.cpp.o.d"
+  "CMakeFiles/Project.dir/ProductHandler.cpp.o"
+  "CMakeFiles/Project.dir/ProductHandler.cpp.o.d"
+  "CMakeFiles/Project.dir/main.cpp.o"
+  "CMakeFiles/Project.dir/main.cpp.o.d"
+  "Project"
+  "Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
